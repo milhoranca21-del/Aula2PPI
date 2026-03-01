e0e917614c2564a180039469bb3476054dee7f6c
milhoranca21-del <milhoranca21@gmail.com>
2026-03-01 10:32:58 -0300

    arthur

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const host = '0.0.0.0' //todas interfaces de rede pode acessar a nossa aplicaçao
+
+const porta = 3001; //aplicaçao indentificada pelo numero 3000
+
+const app = express();
+
+app
